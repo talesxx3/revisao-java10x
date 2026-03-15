9936b6c2aeea7884540b0f6cbad6ab406764d986
talesxx3 <tales_silva2013@live.com>
2026-03-14 23:29:15 -0300

    feat: Aula sobre tipos primitivos

---
+++ src/Introducao/DadosNaoPrimitivos/Main.java
@@ -0,0 +1,11 @@
+package Introducao.DadosNaoPrimitivos;
+
+public class Main {
+    public static void main(String[] args) {
+        // Sao tipos de dados em que pode-se colocar metodos para fazer alteracoes na variavel sem que seja mudado seu escopo.
+        // Dados nao primitivos - String, Array, Class, enum
+
+        String nome = "Michael Jordan";
+        String nomeUpperCase = nome.toUpperCase();
+    }
+}
---
+++ src/Introducao/DadosPrimitivos/Main.java
@@ -0,0 +1,18 @@
+package Introducao.DadosPrimitivos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Sao tipos de dados que de maneira geral nao recebem metodos de maneira padrao.
+        // Dados primitivos - int, double, float, char, boolean, short.
+
+        int idade = 18;
+        char sexo = 'M';
+        double altura = 1.88;
+        float salario = 200000;
+        boolean cantorBom = true;
+
+
+
+    }
+}
